b36560ef26828c32ed400d2704cc238be7362f41
Adan Carrasco <adan.carrasco@adeccogroup.com>
2022-04-14 14:44:43 +0200

    chore(django) - Add Django App

---
+++ djangoapp-mod/env.ps1
@@ -0,0 +1,4 @@
+$Env:DBHOST = "pdb-eur-ww-dev-mi"
+$Env:DBUSER = "admin_"
+$Env:DBNAME = "pollsdb"
+$Env:DBPASS = "Welcome1!"
---
+++ djangoapp-mod/env.sh
@@ -0,0 +1,5 @@
+export DBHOST="pdb-eur-ww-dev-mi"
+export DBUSER="admin_"
+export DBNAME="pollsdb"
+export DBPASS="Welcome1!"
+
